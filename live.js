79637dc20501a09147856bdcb4bbf53a9377befb
kfirmanty <karol.firmanty@gmail.com>
2021-07-27 14:53:45 +0200

    server lang version of mvm

--- live.js
+++ live.js
@@ -4,7 +4,9 @@ const scheduler = require("./components/scheduler.js");
 const parser = require("./parser.js");
 const vm = require("./vm.js");
 
-const parsed = parser.parseMachines("{0.128}");
+const ui = require("./ui/live.js");
+const defaultCode = "{0.4}";
+const parsed = parser.parseMachines(defaultCode);
 const machines = Object.fromEntries(Object.keys(parsed).map(k => [k, vm.init(parsed[k])]));
 const midiPort = midi.start({});
 const clock = scheduler.start({ singleBarTimeMs: 1000 });
@@ -14,8 +16,30 @@ const run = async (system, machine) => {
     await vm.run(system, machine);
     run(system, machine)
 };
+/*
+let input = defaultCode;
 
 Object.keys(machines).forEach(k => run(system, machines[k]));
+
+const readCode = () => {
+    ui.init(input, (code) => {
+        console.log("received new code", code);
+        input = code;
+        let newMachinesCode = parser.parseMachines(input);
+        Object.keys(newMachinesCode).forEach(k => {
+            if (machines[k]) {
+                machines[k].commands = newMachinesCode[k];
+            } else {
+                let machine = vm.init(newMachinesCode[k]);
+                machines[k] = machine;
+                run(system, machine);
+            }
+        });
+        readCode();
+    });
+}
+readCode();
+*/
 
 const rl = readline.createInterface({
     input: process.stdin,
---
+++ server.js
@@ -0,0 +1,41 @@
+const ws = require("ws");
+const midi = require("./components/midi.js");
+const scheduler = require("./components/scheduler.js");
+const parser = require("./parser.js");
+const vm = require("./vm.js");
+
+const defaultCode = "{0.4}";
+const parsed = parser.parseMachines(defaultCode);
+const machines = Object.fromEntries(Object.keys(parsed).map(k => [k, vm.init(parsed[k])]));
+const midiPort = midi.start({});
+const clock = scheduler.start({ singleBarTimeMs: 1000 });
+const system = { midi: midiPort, clock };
+
+const run = async (system, machine) => {
+    await vm.run(system, machine);
+    run(system, machine)
+};
+
+Object.keys(machines).forEach(k => run(system, machines[k]));
+
+let code = defaultCode;
+const wss = new ws.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    console.log("new ws conn");
+    ws.on('message', (message) => {
+        console.log('received: %s', message);
+        message = JSON.parse(message);
+        code = message.code;
+        let newMachinesCode = parser.parseMachines(code);
+        Object.keys(newMachinesCode).forEach(k => {
+            if (machines[k]) {
+                machines[k].commands = newMachinesCode[k];
+            } else {
+                let machine = vm.init(newMachinesCode[k]);
+                machines[k] = machine;
+                run(system, machine);
+            }
+        });
+    });
+});
